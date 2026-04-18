68568c004f11e681a0446ac49e81c77ad89c232d
henri-32 <henrikirchhoff@gmail.com>
2026-04-18 15:54:00 +0200

    File Struktur geändert

---
+++ LeetCode/mergeStrings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+
+public:
+  std::string mergeAlternately(std::string word1, std::string word2) {
+    return word2;
+  }
+};
+
+Solution solution; 
+
+int main () {
+	std::cout<<solution.mergeAlternately("Hello", "Henri");
+
+}; 
